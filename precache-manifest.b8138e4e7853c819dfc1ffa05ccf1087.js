2870f1e008e9a626aadb430bb76bccdd79f62fa0
ventisca6201 <adisak.kongwon@gmail.com>
2020-06-17 17:29:27 +0700

    Updates

---
+++ precache-manifest.b8138e4e7853c819dfc1ffa05ccf1087.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "624924f112b5610c7c0b64af11fde8df",
+    "url": "/workerTest/index.html"
+  },
+  {
+    "revision": "e05252802cea94426dc2",
+    "url": "/workerTest/static/css/2.30c2d978.chunk.css"
+  },
+  {
+    "revision": "ad71b4aa1acd9cab98fd",
+    "url": "/workerTest/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e05252802cea94426dc2",
+    "url": "/workerTest/static/js/2.d06d2145.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/workerTest/static/js/2.d06d2145.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad71b4aa1acd9cab98fd",
+    "url": "/workerTest/static/js/main.ca08f5dc.chunk.js"
+  },
+  {
+    "revision": "217214481f35fffb84e3",
+    "url": "/workerTest/static/js/runtime-main.f5a40976.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/workerTest/static/media/logo.5d5d9eef.svg"
+  }
+]);
